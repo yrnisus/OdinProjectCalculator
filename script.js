224b35d2b560d0ba395fa96291937425f482f0bf
yrnisus <cjmckeefry@Yahoo.com>
2022-04-06 10:38:26 -0400

    Basic math functions started

--- script.js
+++ script.js
@@ -1,25 +1,75 @@
 
-let value = '';
-let total = '';
+let value = 0;
+let valueString = '';
+let total = 0;
+let totalString = '';
+let firstOperator = true;
 
 // Button Listeners
 const numButtons = document.querySelectorAll('#num').forEach(item => {
-    // On click color store the value of the button
+    // On click store the valueString of the button
     item.addEventListener('mousedown', event => {
-        value += '' + item.innerHTML;
+        valueString += '' + item.innerHTML;
         displayNum();
     })
 })
 
+// Operator button listeners
 const operatorBtns = document.querySelectorAll('#operator').forEach(item => {
-    // On click color store the value of the button
+    // On click store the valueString of the button
     item.addEventListener('mousedown', event => {
-        total = value;
-        displayTotal();
-        value = '';
+        let operator = item.innerHTML;
+        //do math first
+        operate(operator);
+        displaytotalString();
+        valueString = '';
         displayNum();
+        firstOperator = false;
     })
+
 })
+
+// perform operations
+function operate(operator) {
+    if (totalString !== '')
+        total = parseInt(totalString, 10);
+    value = parseInt(valueString, 10);
+
+    switch (operator) {
+        case '+':
+            console.log('plus');
+            total += value;
+            console.log(total);
+            break;
+        case '-':
+            console.log('subtract');
+            if (firstOperator) {
+                total = value;
+                break
+            }
+            total -= value;
+            break;
+        case '×':
+            console.log('x');
+            if (firstOperator) {
+                total = value;
+                break
+            }
+            total *= value;
+            break;
+        case '÷':
+            console.log('/');
+            if (firstOperator) {
+                total = value;
+                break
+            }
+            total /= value;
+            break;
+    }
+
+    totalString = String(total);
+
+}
 
 
 
@@ -27,22 +77,31 @@ const equalBtn = document.getElementById('equal');
 
 // Click equal btn
 equalBtn.addEventListener('mousedown', event => {
-    console.log(value);
+    console.log(valueString);
 })
 
 // Click clear btn
 const clearBtn = document.getElementById('clear');
 clearBtn.addEventListener('mousedown', event => {
-    value = '';
-    total = '';
+    valueString = '';
+    totalString = '';
+    total = 0;
+    value = 0;
+    firstOperator = true;
     displayNum();
+    displaytotalString();
 })
 
 
+
+
+
+// display functions
+
 function displayNum() {
-    document.getElementById('outputText').innerHTML = value;
+    document.getElementById('outputText').innerHTML = valueString;
 }
 
-function displayTotal() {
-    document.getElementById('totalText').innerHTML = total;
+function displaytotalString() {
+    document.getElementById('totalText').innerHTML = totalString;
 }
